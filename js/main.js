4fd45441c436a39ddb126014def9abbc0969c790
Robin <github@missbossy.com>
2013-01-22 17:13:32 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// console.log("main.js is loaded");
+$(document).ready(function(){
+	$("button").on("click", function(){
+		$(".booboo").html(
+			$("textarea").val()
+		);
+	});
+
+	$("li").on("click", function(){
+		
+		if ($(this).hasClass("done")) {
+			$(this).removeClass("done");
+		} else {
+			$(this).addClass("done");
+		}
+	});
+
+
+	$("img").on("dblclick", function(){
+		//here.
+		$("img").width(200);
+
+		$("h1").fadeOut();
+
+		$(".booboo").slideUp();
+
+		$("#rahul").hide();
+	});
+
+	// $("img").click(function(){
+	// 	//here.
+	// 	$("img").width(200);
+	// });
+});
